e8b8b374c5f870cae15c47bde332cb9a77ded81d
arnabw <arnabsahawrk@gmail.com>
2024-01-28 23:32:14 +0600

    completed task2

---
+++ add_or_remove.js
@@ -0,0 +1,17 @@
+/* 2. Add or remove elements
+* Declare an array of 3 tourist destinations
+* Add a new tourist destination to your tourist array
+* Add two more to your array
+* Remove the last tourist destination you have added
+* display the final array as output */
+
+let places = ["Sajek", "Bandarban", "Cox's Bazar"];
+
+places.push("Kuakata");
+console.log(places);
+
+places.push("India", "China");
+console.log(places);
+
+places.pop();
+console.log(places);
