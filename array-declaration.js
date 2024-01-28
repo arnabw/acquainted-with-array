2febd18ea5a8bd17ba7458b931a1a24cc66d843c
arnabw <arnabsahawrk@gmail.com>
2024-01-28 23:25:12 +0600

    completed task1

---
+++ array-declaration.js
@@ -0,0 +1,11 @@
+/* 1. Declare an array
+* Declare an array with 5 elements containing fruits
+* console log the 3rd index element
+* change the value of the 2nd index element to Pomelo
+* console log the final array */
+
+let fruits = ["Apple", "Banana", "Guava", "Pomegranate", "Grape"];
+console.log(fruits[3]);
+
+fruits[2] = "Pomelo";
+console.log(fruits);
