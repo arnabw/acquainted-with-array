777bd6fde9988e2eba63b8facf100e07a411decc
arnabw <arnabsahawrk@gmail.com>
2024-01-29 00:05:52 +0600

    completed task5

---
+++ combinigArray.js
@@ -0,0 +1,15 @@
+/* 5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log(). */
+
+let myName = ["Arnab", "Emu", "Tapas", "Niloy"];
+let myFamily = ["Arna", "Maitry", "Arup"];
+
+console.log(myName);
+console.log(myFamily);
+
+let concat = myName.concat(myFamily);
+console.log(concat);
